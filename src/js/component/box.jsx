0bf0458dc04a8ce185a6285b797e51a6c79125c3
BrayanSO <briansolera@gmail.com>
2022-11-23 01:59:04 +0000

    segundo

--- src/js/component/box.jsx
+++ src/js/component/box.jsx
@@ -1,16 +1,19 @@
 import React from 'react';
 import ListGroup from 'react-bootstrap/ListGroup';
+import Form from 'react-bootstrap/Form';
 
 function Box() {
   return (
     <div className='container'>
-    <ListGroup>
+      <Form.Group className="mb-3">
+        <Form.Label className='d-flex justify-content-center'>{<h1>todos</h1>}</Form.Label>
+        <Form.Control placeholder="Que necesitas hacer?" />
+      <ListGroup>
       <ListGroup.Item>Cras justo odio</ListGroup.Item>
       <ListGroup.Item>Dapibus ac facilisis in</ListGroup.Item>
       <ListGroup.Item>Morbi leo risus</ListGroup.Item>
-      <ListGroup.Item>Porta ac consectetur ac</ListGroup.Item>
-      <ListGroup.Item>Vestibulum at eros</ListGroup.Item>
     </ListGroup>
+    </Form.Group>
     </div>
   );
 }
--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -2,17 +2,12 @@ import React from "react";
 import Box from "./box.jsx";
 
 //include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Todos</h1>
-			<Box></Box>
-			<p>
-				<img src={rigoImage} />
-			</p>
+		<div className="mt-5">
+			<Box></Box>			
 		</div>
 	);
 };
