9d633755444e35a501fcbcf68f14591b0fe37e79
BrayanSO <briansolera@gmail.com>
2022-11-23 17:53:40 +0000

    casi listo

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,15 +1,28 @@
-import React from "react";
-import Box from "./box.jsx";
-
-//include images into your bundle
-
-//create your first component
+import React, {useState} from 'react';
 const Home = () => {
-	return (
-		<div className="mt-5">
-			<Box></Box>			
-		</div>
-	);
-};
+const [tasks, setTasks]= useState ([
+    "lavar ropa",
+    "bañar al perro"
+  ])
+  const [newTask, setNewTask]= useState("")
+  return (
+  <div className="mb-3 container-fluid d-flex mt-5 justify-content-center ">
+    <ul className="list-group w-25 ">
+    <li className="list-group-item d-flex justify-content-between align-items-center">
+        <input type="text" className='fo' onChange={e=>setNewTask(e.target.value)} value={newTask} name="task" id="task" />
+      </li>
+    {tasks.map(task=>(
+        <li className="list-group-item d-flex justify-content-between align-items-center">
+        {task}
+        <span className="badge bg-primary rounded-pill">x</span>
+      </li>
+    ))}
+	<li className="list-group-item d-flex disabled justify-content-center align-items-center">
+		 <small>{tasks.length} Tareas</small>
+		</li>
+</ul>
+</div>
+  );
+}
 
 export default Home;
