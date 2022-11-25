9f65a7492ecc06fd3645fb1ae8d8997ff7735db9
BrayanSO <briansolera@gmail.com>
2022-11-25 02:54:33 +0000

    falta

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,21 +1,48 @@
-import React, {useState} from 'react';
+import React, {useState,useEffect} from 'react';
 const Home = () => {
 const [tasks, setTasks]= useState ([
-    "Lavar ropa",
-    "Lavar el Auto"
+  {label: "Lavar ropa", done: false},
+  {label: "Lavar el Auto", done: false}
   ])
   const [newTask, setNewTask]= useState("")
   function addTask (e){
 	if (e.code=="Enter" && newTask!=""){
-		setTasks([...tasks, newTask])
+		setTasks([...tasks, {label: newTask, done: false}])
 		setNewTask("")
 	}
   }
+  useEffect(async() =>{
+      let respuesta = await fetch("https://assets.breatheco.de/apis/fake/todos/user/Brayan")
+      if (respuesta.status==404){
+        // crear la lista
+        respuesta =await fetch("https://assets.breatheco.de/apis/fake/todos/user/Brayan"),{
+          method:"POST",
+          body:JSON.stringify([]),
+          headers:{
+            Content-Type: "application/json"
+          }
+          respuesta =await fetch("https://assets.breatheco.de/apis/fake/todos/user/Brayan")
+        } else if (!respuesta.ok){
+          // errror
+          console.error("error al cargar"+respuesta.statusText)
+        }
+      }
+      var data=await respuesta.json()
+      setTasks(data)
+      // cargar la lista
+  },[])
   function removeTask(index){
     let newTasks= [...tasks]
     newTasks.splice(index,1)
     setTasks(newTasks)
   }
+  useEffect(async () =>{
+    respuesta =await fetch("https://assets.breatheco.de/apis/fake/todos/user/Brayan"),{
+      method:"PUT",
+      body:JSON.stringify([tasks]),
+      headers:{
+        "content-type":"application/json"}
+  },[tasks])
   return (
   <div className="mb-3 container-fluid d-flex mt-5 justify-content-center ">
     <ul className="list-group w-35 justify-content-center ">
@@ -24,7 +51,7 @@ const [tasks, setTasks]= useState ([
       </li>
     {tasks.map((task, index)=>(
         <li key={index} className="list-group-item d-flex justify-content-between align-items-center">
-        {task}
+        {task.label}
         <button onClick={()=>removeTask(index)} className="badge bg-primary rounded-pill justify-content-center">x</button>
       </li>
     ))}
